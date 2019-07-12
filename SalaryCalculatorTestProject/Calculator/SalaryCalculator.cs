2de57223dd222466775794f8d89ab5c40c06d616
Kwee T <kweetang@gmail.com>
2019-07-12 15:58:30 -0700

    Refactor methods with lambdas

--- SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
+++ SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
@@ -9,16 +9,9 @@ namespace Calculator
     public class SalaryCalculator
     {
         const int HoursInYear = 2080;
-
-        public decimal GetAnnualSalary(decimal hourlyWage)
-        {   
-            decimal annualSalary = hourlyWage * HoursInYear;
-            return annualSalary;
-        }
-
-        public decimal GetHourlyWage(int annualSalary)
-        {
-            return annualSalary/HoursInYear;
-        }
+    
+        public decimal GetAnnualSalary(decimal hourlyWage) => hourlyWage * HoursInYear;
+     
+        public decimal GetHourlyWage(int annualSalary) => annualSalary / HoursInYear; 
     }
 };
